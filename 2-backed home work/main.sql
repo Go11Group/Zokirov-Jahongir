8e1c2dd23d7028f2d9d71065b0ca01616ca0302e
Jahongir0101 <jahongirzokirov580@gmail.com>
2024-06-08 19:52:25 +0500

    Add files via upload

---
+++ 2-backed home work/main.sql
@@ -0,0 +1,25 @@
+INSERT INTO author (name, bio) VALUES
+('Author 1', 'Bio of Author 1'),
+('Author 2', 'Bio of Author 2'),
+('Author 3', 'Bio of Author 3'),
+('Author 4', 'Bio of Author 4'),
+('Author 5', 'Bio of Author 5'),
+('Author 6', 'Bio of Author 6'),
+('Author 7', 'Bio of Author 7');
+
+INSERT INTO book (title, publication_date, author_id) VALUES
+('Book 1', '2023-01-01', 1),
+('Book 2', '2023-02-01', 1),
+('Book 3', '2023-03-01', 2),
+('Book 4', '2023-04-01', 2),
+('Book 5', '2023-05-01', 3),
+('Book 6', '2023-06-01', 3),
+('Book 7', '2023-07-01', 4),
+('Book 8', '2023-08-01', 4),
+('Book 9', '2023-09-01', 5),
+('Book 10', '2023-10-01', 5),
+('Book 11', '2023-11-01', 6),
+('Book 12', '2023-12-01', 6),
+('Book 13', '2024-01-01', 7),
+('Book 14', '2024-02-01', 7),
+('Book 15', '2024-03-01', 1);
